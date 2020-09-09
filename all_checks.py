e62f83c1bffa34adfbb8aa2f4debd7813b01fbf6
Ajeet Prakash <nobleprizewinners@gmail.com>
2020-09-09 14:29:35 +0530

    fir se

--- all_checks.py
+++ all_checks.py
@@ -1,3 +1,2 @@
 #this is all_checks.py
-print("this is all_checks.py in master  branch in the local repo health-checks2")
-print("hello world")
+print("this is all_checks.py in refactor  branch in the local repo health-checks2")
